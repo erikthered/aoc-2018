2eac37294c2170524057183c6d8a53914a4e32da
Erik Nelson <erik.david.nelson@gmail.com>
2018-12-04 12:33:15 -0500

    Refactor Day 1 Part 2 to include file read in timing block

--- src/main/kotlin/aoc/day1/Part2.kt
+++ src/main/kotlin/aoc/day1/Part2.kt
@@ -5,16 +5,16 @@ import java.io.File
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    val input = File(getResourceUri("input1.txt")).bufferedReader()
-            .lineSequence().map { it.toInt() }.toList()
-
-    val changeSeq = generateSequence { input.asIterable() }.flatten()
-
-    var currFreq = 0
-    val freqs = mutableSetOf<Int>()
-    var iters = 0
 
     val timeTaken = measureTimeMillis {
+        val input = File(getResourceUri("input1.txt")).bufferedReader()
+                .lineSequence().map { it.toInt() }.toList()
+
+        val changeSeq = generateSequence { input.asIterable() }.flatten()
+
+        var currFreq = 0
+        val freqs = mutableSetOf<Int>()
+        var iters = 0
         changeSeq
                 .map {
                     currFreq += it
@@ -26,9 +26,9 @@ fun main(args: Array<String>) {
                     iters++
                 }
 
+        println("first repeated freq: $currFreq")
+        println("iterations: $iters")
     }
 
-    println("first repeated freq: $currFreq")
-    println("iterations: $iters")
     println("time taken: ${timeTaken}ms")
 }
