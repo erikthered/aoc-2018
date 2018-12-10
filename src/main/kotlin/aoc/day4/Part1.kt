242dd31d2af2f9a355dbecebd22cf79fb1743281
Erik Nelson <erik.david.nelson@gmail.com>
2018-12-10 16:05:18 -0500

    Add Day 4 Part 1 solution
    I really need to fix all the nullable double bangs

---
+++ src/main/kotlin/aoc/day4/Part1.kt
@@ -0,0 +1,66 @@
+package aoc.day4
+
+import aoc.getResourceUri
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    val timeTaken = measureTimeMillis {
+        val input = File(getResourceUri("input4.txt")).bufferedReader().readLines()
+
+        val regex = """\[(.*)\]\s(.*)""".toRegex()
+        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+
+        val shiftChangeRegex = """Guard #(\d+) begins shift""".toRegex()
+
+        val minutesAsleep = mutableMapOf<Int, List<Int>>()
+
+        val eventLog = input.map {
+            val (ts, text) = regex.find(it)!!.destructured
+            PatrolEvent(LocalDateTime.parse(ts, dateFormat), text)
+        }
+                .sortedBy { it.timestamp }
+
+        var currentId: Int = 0
+        var startAsleep: Int = 0
+        var endAsleep: Int = 0
+
+        eventLog.forEach {
+            when {
+                shiftChangeRegex.matches(it.event) -> {
+                    currentId = shiftChangeRegex.find(it.event)!!.groupValues[1].toInt()
+                }
+                it.event == "falls asleep" -> {
+                    startAsleep = it.timestamp.minute
+                }
+                it.event == "wakes up" -> {
+                    endAsleep = it.timestamp.minute
+
+                    val minutes = startAsleep until endAsleep
+
+                    if (minutesAsleep.containsKey(currentId)) {
+                        minutesAsleep[currentId] = minutesAsleep[currentId]!!.plus(minutes)
+                    } else {
+                        minutesAsleep[currentId] = minutes.toList()
+                    }
+                }
+            }
+        }
+
+        val (id, minutes) = minutesAsleep.maxBy { it.value.size }!!
+
+        println("$id: ${minutes.size}")
+
+        val (minute, occurrences) = minutes.groupingBy { it }.eachCount().maxBy { it.value }!!
+
+        println("Minute: $minute, $occurrences times")
+
+        println("Checksum: ${id * minute}")
+
+    }
+    println("time taken: ${timeTaken}ms")
+}
+
+data class PatrolEvent(val timestamp: LocalDateTime, val event: String)
