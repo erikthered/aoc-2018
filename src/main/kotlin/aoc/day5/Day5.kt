f959fba9d2c17d294ac67194fad2521ab30eb421
Erik Nelson <erik.david.nelson@gmail.com>
2019-05-08 16:02:36 -0400

    Solve Day 5 part 2

--- src/main/kotlin/aoc/day5/Day5.kt
+++ src/main/kotlin/aoc/day5/Day5.kt
@@ -7,7 +7,13 @@ import java.io.File
 fun main() {
     val input = File(getResourceUri("input5.txt")).bufferedReader().readLines()
 
-    println(react(input[0]).length)
+    println("Result for part 1: ${react(input[0]).length}")
+
+    val (letter, size) = CharRange('a', 'z')
+            .map { Pair(it, react(input[0].filterNot { c -> c.equals(it, true) }).length) }
+            .minBy { it.second }!!
+
+    println("Result for part 2: $letter => $size")
 }
 
 fun react(input: String): String {
--- src/test/kotlin/aoc/day5/Day5Test.kt
+++ src/test/kotlin/aoc/day5/Day5Test.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 import kotlin.test.assertEquals
 
 
-class Part1Test {
+class Day5Test {
 
     @Test
     fun testCollapse() {
